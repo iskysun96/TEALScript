f1e12ccc907315a255f1ae0c799fe989c7e6e77a
Chris Kim <chris.kim@algorand.com>
2023-08-07 13:43:50 -0700

    added descriptions for each method and states

--- examples/rsvp/rsvp.algo.ts
+++ examples/rsvp/rsvp.algo.ts
@@ -8,45 +8,73 @@ const MIN_BAL = 100_000;
 
 // eslint-disable-next-line no-unused-vars
 class EventRSVP extends Contract {
+  /** The price of the ticket to rsvp (uALGO) */
   price = new GlobalStateKey<uint64>()
 
+  /** The total number of people who rsvp'd */
   rsvp = new GlobalStateKey<uint64>()
 
-  checked_in = new LocalStateKey<uint64>()
+  /** Boolean value indicating if the account checked in or not */
+  checked_in = new LocalStateKey<Boolean>()
 
   // checkOptedIn(): void {
   //   this.app.
   //   assert(this.txn.sender. )
 
+  /**
+   * Check if Txn sender is the creator of the contract
+   */
   authorizeCreator(): void {
     assert(this.txn.sender === this.app.creator)
   }
 
-  @handle.createApplication
+  /**
+   * Create the RSVP contract
+   * 
+   * @param eventPrice The price of the ticket to rsvp (uALGO) 
+   * 
+   */
   create(eventPrice: uint64): void {
     this.rsvp.set(0)
     this.price.set(eventPrice)
   }
 
-  @handle.optIn
+  /**
+   * Purchase a ticket to the event and opt in to 
+   * the contract. Can only call once per account.
+   * 
+   * @param payment a payment transaction object that is atomically grouped with the opt-in transaction
+   * 
+   */
+  @allow.call("OptIn")
   doRsvp(payment: PayTxn): void {
     assert(globals.groupSize === 2)
     assert(payment.receiver === this.app.address)
     assert(payment.amount === this.price.get())
+    assert(!this.checked_in.exists(this.txn.sender))
 
-    this.checked_in.set(this.txn.sender, 0)
+    this.checked_in.set(this.txn.sender, false)
     this.rsvp.set(this.rsvp.get() + 1)
   }
 
+  /**
+   * Check in to the event
+   */
   checkIn(): void {
-    this.checked_in.set(this.txn.sender, 1)
+    this.checked_in.set(this.txn.sender, true)
   }
 
+  /**
+   * Withdraw funds from the contract using the 
+   * internal withdrawFunds subroutine
+   */
   withdrawExternal(): void {
     this.withdrawFunds()
   }
 
-
+  /**
+   * Internal Subroutine that withdraw funds from the contract
+   */
   private withdrawFunds(): void {
     const rsvpBal = this.app.address.balance
     assert(rsvpBal > MIN_BAL + FEE)
@@ -61,27 +89,47 @@ class EventRSVP extends Contract {
    * Read Methods
    */
 
+  /**
+   * A read only method that gets how many people rsvp'd
+   * 
+   * @returns the number of people who rsvp'd
+   */
+  @abi.readonly(true)
   readRsvp(): uint64 {
     this.authorizeCreator()
     return this.rsvp.get()
   }
 
-  @abi.readonly
+  /**
+   * A read only method that gets the price of the ticket
+   * 
+   * @returns the price of the ticket
+   */
+  @abi.readonly(true)
   readPrice(): uint64 {
     return this.price.get()
   }
 
-  @handle.closeOut
+  /**
+   * Refunds the sender the price of the ticket 
+   * when the sender closes out of the contract
+   */
+  @allow.call("CloseOut")
   refund(): void {
     this._doRefund()
   }
 
-
-  @handle.clearState
+  /**
+   * Refunds the sender the price of the ticket 
+   * when the sender sends a ClearState App call.
+   */
   clearState(): void {
     this._doRefund()
   }
 
+  /**
+   * Internal method that refunds the sender the price of the ticket
+   */
   _doRefund(): void {
     sendPayment({
       amount: this.price.get() - FEE,
